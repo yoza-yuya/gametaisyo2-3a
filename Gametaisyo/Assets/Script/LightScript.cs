57677fae378021216571fcbd12b544f003a66928
KBC12005185\kbc12005185 <kana5665>
2022-02-17 14:21:12 +0900

    ライトが回転するだけ

--- Gametaisyo/Assets/Script/LightScript.cs
+++ Gametaisyo/Assets/Script/LightScript.cs
@@ -7,12 +7,11 @@ public class LightScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 120f * Time.deltaTime, 0f);
     }
 }

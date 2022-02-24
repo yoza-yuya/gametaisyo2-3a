2e27f1763405fdf83186e08877a7ee8f588b6586
KBC12006108\kbc12006108 <yuto-tomori-20h@stu.kbc.ac.jp>
2022-02-24 11:58:41 +0900

    sceneの追加

---
+++ Gametaisyo/Assets/Script/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+
+        Time.timeScale = 1f;//戻る
+        Debug.Log("Restart");
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Start");
+        Time.timeScale = 1f;//戻る
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadScene("Help");
+
+    }
+}

e3f8f91b24da6e95b9e32e311b978761b217411f
KBC12005185\kbc12005185 <kana5665>
2022-03-03 15:21:28 +0900

    敵の発見

--- Gametaisyo/Assets/Script/VisualSkill.cs
+++ Gametaisyo/Assets/Script/VisualSkill.cs
@@ -6,28 +6,61 @@ using System;
 public class VisualSkill : MonoBehaviour
 {
     Renderer ren;
+    Light light;
+    private float time = 0f;
+    private int i = 0;
+    public int SkillTime = 5; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (this.gameObject.tag == "Light")
+        {
+            light = GetComponent<Light>();
+            light.enabled = false;
+        }
+        else
+        {
+            ren = GetComponent<Renderer>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Wall")
+        if (Input.GetKeyDown("joystick button 5"))
         {
-            if (Input.GetKeyDown("joystick button 5"))
+            if (this.gameObject.tag == "Light")
             {
-                ren.enabled = false;
+                light.enabled = true;
             }
             else
             {
+                ren.enabled = false;
+            }
+            i = 1;
+        }
+        if (this.gameObject.tag == "Light")
+        {
+            if (i == 1)
+            {
+                time += Time.deltaTime;
+            }
+            if (time >= SkillTime)
+            {
+                light.enabled = false;
+                time = 0f;
+                i = 0;
+            }
+        }
+        else
+        {
+            if (i == 1) {
+                time += Time.deltaTime;
+            }
+            if (time >= SkillTime) {
                 ren.enabled = true;
+                time = 0f;
+                i = 0;
             }
         }
     }

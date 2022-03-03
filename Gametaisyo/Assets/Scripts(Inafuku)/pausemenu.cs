4b0418515c5494d7a522d2b0202128e8e0a71194
KBC12005020\kbc12005020 <junichiro-inafuku-20f@stu.kbc.ac.jp>
2022-03-03 13:50:02 +0900

    ポーズ画面の完成、タイトルシーンの作成、一応仮のタイトル画面の作成途中

---
+++ Gametaisyo/Assets/Scripts(Inafuku)/pausemenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenuCanvas;
+    public GameObject curObj;
+    Cursor cur;
+
+    //int Pauseswich;
+    int Pausecount = 0;
+    //public TimeCounter TimeCounter;
+
+    void Start()
+    {
+        //Pauseswich = TimeCounter.Pozuswich;
+        PauseMenuCanvas.SetActive(false);
+        //cur = curObj.GetComponent<Cursor>();
+    }
+
+    void Update()
+    {
+        bool start = Input.GetKeyDown("joystick button 7");
+
+        if (start == true && Pausecount == 0)
+        {
+            Time.timeScale = 0;
+            PauseMenuCanvas.SetActive(true);
+            Pausecount = 1;
+
+        }
+        else if (start == true && Pausecount == 1)
+        {
+            Time.timeScale = 1;
+            PauseMenuCanvas.SetActive(false);
+            Pausecount = 0;
+
+        }
+
+        //if (pausePanel.activeSelf == false)
+        //{
+        //    cur.enabled = false;
+        //}
+        //else
+        //{
+        //    cur.enabled = true;
+        //}
+    }
+}

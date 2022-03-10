9f149036d37aa6d2e7d8b5354191c91482201647
Ushiku-Hiroto <hiroto_usiku20f@stu.kbc.ac.jp>
2022-03-10 10:59:20 +0900

    アイテムを取るとゴールが消えるスクリプトの完成

---
+++ Gametaisyo/Assets/Script/GoalOpen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalOpen : MonoBehaviour
+{
+    int GoalCount = 0;
+    public GameObject Goal;
+    public void Goalflg()
+    {
+        //アイテムが取られるとカウントを増やす
+        GoalCount++;
+        Debug.Log(GoalCount);
+        //五つ取ったら
+        if(GoalCount == 5)
+        {
+            Goal.SetActive(false);
+        }
+    }
+}

bade2ad8ecbfcc82e9b792a48186432b7ab97eca
Ushiku-Hiroto <hiroto_usiku20f@stu.kbc.ac.jp>
2022-04-07 12:06:02 +0900

    感覚アイテムの切り替えスクリプトの作成：牛久大翔

---
+++ Gametaisyo/Assets/Script/Skill List.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillList : MonoBehaviour
+{
+    int[] SkillLists = new int[5];  //五感の配列
+    bool SkillGetflg = false;   //感覚を手に入れているかのフラグ
+    bool SkillExecutionflg = false; //感覚を使ったかのフラグ
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

499b6554859a60e87ee295638dc27550c26dcb34
KBC12005185\kbc12005185 <kana5665>
2022-04-14 10:12:32 +0900

    エネミーがplayerを探知するスクリプト

---
+++ Gametaisyo/Assets/Script/EnemyDetection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public bool flag = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            flag = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            flag = false;
+        }
+    }
+}

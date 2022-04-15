95514daf09ea448a242494385fb0ebad95bfc007
yoza-yuya <yuuya-yoza-20f@stu.kbc.ac.jp>
2022-04-15 12:55:41 +0900

    アイテム欄の作り直し

---
+++ Gametaisyo/Assets/Script/ItemMoveUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemMoveUI : MonoBehaviour
+{
+    public RectTransform a;//RectTransform型の変数
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.position += new Vector3(0.1f, 0, 0);
+    }
+
+}

606bcae0a4df1f3c9d37fdfd7ff6afca6d472f2a
Ushiku-Hiroto <hiroto_usiku20f@stu.kbc.ac.jp>
2022-04-15 12:45:20 +0900

    アイテム欄プレハブ：牛久大翔

--- Gametaisyo/Assets/Script/AtkPlayer.cs
+++ Gametaisyo/Assets/Script/AtkPlayer.cs
@@ -1,42 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class AtkPlayer : MonoBehaviour
-{
-    private Syokkaku Syokkaku;
-    private Mikaku Mikaku;
+//public class AtkPlayer : MonoBehaviour
+//{
+//    private Syokkaku Syokkaku;
+//    private Mikaku Mikaku;
 
-    private Itemselect sw;
+//    private Itemselect sw;
 
-    void Start()
-    {
-        Syokkaku = GetComponent<Syokkaku>();
-        Mikaku = GetComponent<Mikaku>();
-        GameObject canvas = GameObject.Find("Canvas");
-        sw = canvas.GetComponent<Itemselect>();
-    }
+//    void Start()
+//    {
+//        Syokkaku = GetComponent<Syokkaku>();
+//        Mikaku = GetComponent<Mikaku>();
+//        GameObject canvas = GameObject.Find("Canvas");
+//        sw = canvas.GetComponent<Itemselect>();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float dx = Input.GetAxis("Horizontal");
-        float dy = Input.GetAxis("Vertical");
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        float dx = Input.GetAxis("Horizontal");
+//        float dy = Input.GetAxis("Vertical");
 
-        /*
-         * 武器選択UIで選択した武器で攻撃する
-         */
-        switch (sw.WeponType)
-        {
-            case 0:
-                knife.Attack(dx, dy, sources[0]);
-                break;
-            case 1:
-                hundGun.Attack(dx, dy, sources[1]);
-                break;
-            case 2:
-                knife.Attack(dx, dy, sources[0]);
-                break;
-        }
-    }
-}
+//        /*
+//         * 武器選択UIで選択した武器で攻撃する
+//         */
+//        switch (sw.WeponType)
+//        {
+//            case 0:
+//                knife.Attack(dx, dy, sources[0]);
+//                break;
+//            case 1:
+//                hundGun.Attack(dx, dy, sources[1]);
+//                break;
+//            case 2:
+//                knife.Attack(dx, dy, sources[0]);
+//                break;
+//        }
+//    }
+//}

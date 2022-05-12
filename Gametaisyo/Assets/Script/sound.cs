5dff1e89a275a480157e7371ddffe84d62a8f279
KBC12005157\kbc12005157 <haruto-uchima-20f@stu.kbc.ac.jp>
2022-05-12 14:15:01 +0900

    sound　作成

--- Gametaisyo/Assets/Script/sound.cs
+++ Gametaisyo/Assets/Script/sound.cs
@@ -7,12 +7,9 @@ public class sound : MonoBehaviour
 {
     // ぶつかった時の音
     public AudioClip se;
-    public AudioClip se2;
-    public AudioClip se3;
     // AudioClip再生用
     AudioSource audiosource1;
 
-    int Php = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,25 +20,13 @@ public class sound : MonoBehaviour
     }
     
     // playerにぶつかった時に音を鳴らす
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision hit)
     {
-        if(Php < 5) {
-            if (other.gameObject.name == "Enemy")
+            if (hit.gameObject.name == "Player")
             {
                 audiosource1.PlayOneShot(se);
+                Debug.Log("痛い");
             }
-            Php += -1;
-        }else if (Php < 1) {
-
-            if (other.gameObject.name == "Enemy")
-            {
-                audiosource1.PlayOneShot(se2);
-                SceneManager.LoadScene("GameOberScene");
-            }
-        }
-        if(other.gameObject.name == "Item")
-        {
-            audiosource1.PlayOneShot(se3);
-        }
+        
     }
 }

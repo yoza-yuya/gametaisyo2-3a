51c0dd09858ea620206d8b33f1d331b19e9b1ed7
Ushiku-Hiroto <hiroto_usiku20f@stu.kbc.ac.jp>
2022-05-12 14:40:29 +0900

    嗅覚スクリプトにカメラの切り替えを追加：牛久大翔

--- Gametaisyo/Assets/Script/Tyuukaku.cs
+++ Gametaisyo/Assets/Script/Tyuukaku.cs
@@ -10,9 +10,38 @@ public class Tyuukaku : MonoBehaviour
 	// カーソル
 	[SerializeField] Transform cursor;
 
-	// Update is called once per frame
-	void Update()
+	bool TyuukakuSkillflg = false;    //コルーチンフラグ
+	bool TyuukakuCameraflg = true;
+	private GameObject mainCamera;      //メインカメラ格納用
+	private GameObject subCamera;       //サブカメラ格納用 
+
+    void Start()
+    {
+		//メインカメラとサブカメラをそれぞれ取得
+		mainCamera = GameObject.Find("MiniMapCamera");
+		subCamera = GameObject.Find("TyuukakuMapCamera");
+
+		//サブカメラを非アクティブにする
+		subCamera.SetActive(false);
+	}
+    // Update is called once per frame
+    void Update()
 	{
 		cursor.LookAt(target);
+	}
+
+	IEnumerator SikakuSkill()
+	{
+		TyuukakuSkillflg = true;
+		//サブカメラをアクティブに設定
+		mainCamera.SetActive(false);
+		subCamera.SetActive(true);
+		Debug.Log("やじるし！");
+		yield return new WaitForSeconds(10);
+		//メインカメラをアクティブに設定
+		mainCamera.SetActive(true);
+		subCamera.SetActive(false);
+		TyuukakuSkillflg = false;
+		Debug.Log("消えちゃったやじるし");
 	}
 }
